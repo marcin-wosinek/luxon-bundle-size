d33b49f8f431259a93890be81a9e7a7967bc0ea6
Marcin Wosinek <marcin@wosinek.eu>
2021-07-30 22:14:57 +0200

    add benchmark code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log("test");
+// import trick from https://github.com/moment/luxon/issues/854
+import { DateTime } from "luxon/src/luxon";
+
+console.log("Yesterday was", DateTime.now().minus({ day: 1 }).toJSDate());
